92e377de28c76ac51a7308458c140e2e34f4fb93
JBarti <josip.bartulovic3@skole.hr>
2021-12-13 20:29:49 +0100

    Fix tests

--- tests/test_field.py
+++ tests/test_field.py
@@ -1,11 +1,12 @@
 from models.field import Field, Type
 
-is_bomb = Field()
-is_not_bomb = Field ()
-is_bomb.is_hidden = True
-is_bomb.surrounding_bombs = 3
-is_bomb.type = Type.bomb
-is_not_bomb.is_hidden = False
-is_not_bomb.surrounding_bombs = 1
-is_not_bomb.type = Type.empty
-print(is_bomb.is_hidden)
+def test_field():
+    is_bomb = Field()
+    is_not_bomb = Field ()
+    is_bomb.is_hidden = True
+    is_bomb.surrounding_bombs = 3
+    is_bomb.type = Type.bomb
+    is_not_bomb.is_hidden = False
+    is_not_bomb.surrounding_bombs = 1
+    is_not_bomb.type = Type.empty
+    print(is_bomb.is_hidden)
