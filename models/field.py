6c052e07d3c0c97cdc6ceb30588075e92c7f81f0
DBiukRosic <dbiukr00@fesb.hr>
2021-12-14 00:34:31 +0100

    finished field type

--- models/field.py
+++ models/field.py
@@ -1,16 +1,16 @@
 from typing import List
 from enum import Enum
 
-class Type():
+class Type(Enum):
     bomb = 1
     empty = 0
 
-class Field(Enum):
+class Field():
     def __init__(self,is_hidden: bool, surrounding_bombs: int, type: Type):
         self.is_hidden = is_hidden
         self.surrounding_bombs = surrounding_bombs
         self.type = type
  
     def __repr__(self):
-        field_string = (f"{Field.type} - {Field.is_hidden} - {Field.surrounding_bombs}")
+        field_string = (f"{self.type.name} - {self.is_hidden} - {self.surrounding_bombs}")
         return field_string
