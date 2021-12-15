c34befe90035b3780b30823f1dd48651b66be7a9
DBiukRosic <dbiukr00@fesb.hr>
2021-12-15 21:03:52 +0100

    finished field class

--- test.py
+++ test.py
@@ -1,3 +1,4 @@
 from tests.test_field import test_field
+from models.field import Field, Type
 
 test_field()
--- test_field.py
+++
@@ -1,7 +0,0 @@
-from models.field import Field, Type
-
-bomb_field = Field(True, 6, Type.bomb)
-empty_field = Field (False, 3, Type.empty)
-
-print(bomb_field.__repr__)
-print(empty_field.__repr__)
